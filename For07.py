de735dd86e7842803fbe3cd87d06c99c2382ed81
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-25 07:16:09 +0500

    7

--- For07.py
+++ For07.py
@@ -6,4 +6,12 @@ def main(N):
     Returns:
         int: return  answer
     """
-    return
+    a=list(range(N))
+    i=0
+    ans=0
+    while i<len(a):
+        if a[i]%2==1:
+            ans+=a[i]
+        i+=1    
+    return ans
+print(main(12))    
