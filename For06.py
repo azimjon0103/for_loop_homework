f893f65b455537aedb73fd2117f02914d6713c9a
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-25 14:54:11 +0500

    06

--- For06.py
+++ For06.py
@@ -7,12 +7,8 @@ def main(A,B):
     Returns:
         int: return  answer
     """
-    i=0
-    ans=0
-    a=list(range(A,B))
-    while i<len(a):
-        if a[i]!=0:
-            ans+=a[i]
-        i+=1    
-    return ans
+    b=0
+    for a in range(A,B):
+        b+=a  
+    return b
 print(main(0,5))    
